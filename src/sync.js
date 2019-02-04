f0e5757692de1890cfff4169b50a8abaf4920733
Bruno Windels <brunow@matrix.org>
2019-02-04 22:26:45 +0000

    merge initial & incremental sync

--- src/sync.js
+++ src/sync.js
@@ -1,10 +1,20 @@
-import {parseRooms} from "./common";
-import {RequestAbortError} from "../network";
-import {HomeServerError} from "../error";
+import {RequestAbortError} from "./network.js";
+import {HomeServerError, StorageError} from "./error.js";
 
 const INCREMENTAL_TIMEOUT = 30;
 
-export class IncrementalSync {
+function parseRooms(responseSections, roomMapper) {
+	return ["join", "invite", "leave"].map(membership => {
+		const membershipSection = responseSections[membership];
+		const results = Object.entries(membershipSection).map(([roomId, roomResponse]) => {
+			const room = roomMapper(roomId, membership);
+			return room.processInitialSync(roomResponse);
+		});
+		return results;
+	}).reduce((allResults, sectionResults) => allResults.concat(sectionResults), []);
+}
+
+export class Sync {
 	constructor(network, session, storage) {
 		this._network = network;
 		this._session = session;
