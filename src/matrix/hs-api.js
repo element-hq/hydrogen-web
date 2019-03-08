95bef00054ccdf46b940f8a7a6f93b011366c733
Bruno Windels <brunow@matrix.org>
2019-03-08 20:03:47 +0100

    some comments

--- src/matrix/hs-api.js
+++ src/matrix/hs-api.js
@@ -21,7 +21,9 @@ class RequestWrapper {
 
 export default class HomeServerApi {
 	constructor(homeserver, accessToken) {
-		this._homeserver = homeserver;
+        // store these both in a closure somehow so it's harder to get at in case of XSS?
+        // one could change the homeserver as well so the token gets sent there, so both must be protected from read/write
+        this._homeserver = homeserver;
 		this._accessToken = accessToken;
 	}
 
