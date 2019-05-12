e3328f0fefcd308c780b1a18d17ee512040a681a
Bruno Windels <brunow@matrix.org>
2019-05-12 20:26:32 +0200

    add fragments store name

--- src/matrix/storage/common.js
+++ src/matrix/storage/common.js
@@ -1,4 +1,4 @@
-export const STORE_NAMES = Object.freeze(["session", "roomState", "roomSummary", "roomTimeline"]);
+export const STORE_NAMES = Object.freeze(["session", "roomState", "roomSummary", "timelineEvents", "timelineFragments"]);
 
 export const STORE_MAP = Object.freeze(STORE_NAMES.reduce((nameMap, name) => {
     nameMap[name] = name;
