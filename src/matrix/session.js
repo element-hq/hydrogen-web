10457611f9d35b4659a2b37e4844ec8115451745
Bruno Windels <brunow@matrix.org>
2019-05-12 20:26:46 +0200

    whitespace

--- src/matrix/session.js
+++ src/matrix/session.js
@@ -3,59 +3,59 @@ import { ObservableMap } from "../observable/index.js";
 
 export default class Session {
     // sessionInfo contains deviceId, userId and homeServer
-	constructor({storage, hsApi, sessionInfo}) {
-		this._storage = storage;
+    constructor({storage, hsApi, sessionInfo}) {
+        this._storage = storage;
         this._hsApi = hsApi;
-		this._session = null;
+        this._session = null;
         this._sessionInfo = sessionInfo;
-		this._rooms = new ObservableMap();
+        this._rooms = new ObservableMap();
         this._roomUpdateCallback = (room, params) => this._rooms.update(room.id, params);
-	}
+    }
 
-	async load() {
-		const txn = await this._storage.readTxn([
-			this._storage.storeNames.session,
-			this._storage.storeNames.roomSummary,
-			this._storage.storeNames.roomState,
-			this._storage.storeNames.roomTimeline,
-		]);
-		// restore session object
-		this._session = await txn.session.get();
-		if (!this._session) {
+    async load() {
+        const txn = await this._storage.readTxn([
+            this._storage.storeNames.session,
+            this._storage.storeNames.roomSummary,
+            this._storage.storeNames.roomState,
+            this._storage.storeNames.timelineEvents,
+        ]);
+        // restore session object
+        this._session = await txn.session.get();
+        if (!this._session) {
             this._session = {};
-			return;
-		}
-		// load rooms
-		const rooms = await txn.roomSummary.getAll();
-		await Promise.all(rooms.map(summary => {
-			const room = this.createRoom(summary.roomId);
-			return room.load(summary, txn);
-		}));
-	}
+            return;
+        }
+        // load rooms
+        const rooms = await txn.roomSummary.getAll();
+        await Promise.all(rooms.map(summary => {
+            const room = this.createRoom(summary.roomId);
+            return room.load(summary, txn);
+        }));
+    }
 
     get rooms() {
         return this._rooms;
     }
 
-	createRoom(roomId) {
-		const room = new Room({
+    createRoom(roomId) {
+        const room = new Room({
             roomId,
             storage: this._storage,
             emitCollectionChange: this._roomUpdateCallback,
             hsApi: this._hsApi,
         });
-		this._rooms.add(roomId, room);
-		return room;
-	}
+        this._rooms.add(roomId, room);
+        return room;
+    }
 
-	persistSync(syncToken, accountData, txn) {
-		if (syncToken !== this._session.syncToken) {
-			this._session.syncToken = syncToken;
-			txn.session.set(this._session);
-		}
-	}
+    persistSync(syncToken, accountData, txn) {
+        if (syncToken !== this._session.syncToken) {
+            this._session.syncToken = syncToken;
+            txn.session.set(this._session);
+        }
+    }
 
-	get syncToken() {
-		return this._session.syncToken;
-	}
+    get syncToken() {
+        return this._session.syncToken;
+    }
 }
