843c94b75090479b80dcc9ad2f0501ea74a74adb
Bruno Windels <brunow@matrix.org>
2019-06-01 17:38:23 +0200

    finished observable SortedArray to something useable
    although not as performant as it could be

--- src/observable/list/SortedArray.js
+++ src/observable/list/SortedArray.js
@@ -8,20 +8,32 @@ export default class SortedArray extends BaseObservableList {
         this._items = [];
     }
 
-    setSortedMany(items) {
-
+    setManySorted(items) {
+        // TODO: we can make this way faster by only looking up the first and last key,
+        // and merging whatever is inbetween with items
+        // if items is not sorted, 💩🌀 will follow!
+        // should we check?
+        // Also, once bulk events are supported in collections,
+        // we can do a bulk add event here probably if there are no updates
+        // BAD CODE!
+        for(let item of items) {
+            this.set(item);
+        }
     }
 
-    set(item) {
+    set(item, updateParams = null) {
         const idx = sortedIndex(this._items, item, this._comparator);
-        if (idx < this._items.length || this._comparator(this._items[idx], item) !== 0) {
+        if (idx >= this._items.length || this._comparator(this._items[idx], item) !== 0) {
             this._items.splice(idx, 0, item);
-            //emitAdd
+            this.emitAdd(idx, item)
         } else {
             this._items[idx] = item;
-            //emitRemove
-            //emitAdd
+            this.emitUpdate(idx, item, updateParams);
         }
+    }
+
+    remove(item) {
+        throw new Error("unimplemented");
     }
 
     get array() {
