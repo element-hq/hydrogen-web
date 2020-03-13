af025d68da5948fad999cd4279cbbdce29064a9f
Bruno Windels <brunow@matrix.org>
2020-03-13 23:26:28 +0100

    WIP

--- src/matrix/room/summary.js
+++ src/matrix/room/summary.js
@@ -25,77 +25,105 @@ class ChangeSet {
 
 class SummaryData extends ChangeSet {
 
-    static create(roomId) {
-        return new SummaryData({
-            roomId: roomId,
-            name: null,
-            lastMessageBody: null,
-            unreadCount: null,
-            mentionCount: null,
-            isEncrypted: null,
-            isDirectMessage: null,
-            membership: null,
-            inviteCount: 0,
-            joinCount: 0,
-            readMarkerEventId: null,
-            heroes: null,
-            canonicalAlias: null,
-            aliases: null,
-        });
+    constructor(roomId) {
+        this.roomId = roomId;
+        this.name = null;
+        this.lastMessageBody = null;
+        this.unreadCount = null;
+        this.mentionCount = null;
+        this.isEncrypted = null;
+        this.isDirectMessage = null;
+        this.membership = null;
+        this.inviteCount = 0;
+        this.joinCount = 0;
+        this.readMarkerEventId = null;
+        this.heroes = null;
+        this.canonicalAlias = null;
+        this.aliases = null;
+    }
+
+    clone() {
+        const copy = new SummaryData(this.roomId);
+        copy.name = this.name;
+        copy.lastMessageBody = this.lastMessageBody;
+        copy.unreadCount = this.unreadCount;
+        copy.mentionCount = this.mentionCount;
+        copy.isEncrypted = this.isEncrypted;
+        copy.isDirectMessage = this.isDirectMessage;
+        copy.membership = this.membership;
+        copy.inviteCount = this.inviteCount;
+        copy.joinCount = this.joinCount;
+        copy.readMarkerEventId = this.readMarkerEventId;
+        copy.heroes = this.heroes;
+        copy.canonicalAlias = this.canonicalAlias;
+        copy.aliases = this.aliases;
     }
 
     applySyncResponse(roomResponse, membership) {
+        let changed = false;
         if (roomResponse.summary) {
             this._updateSummary(roomResponse.summary);
+            changed = true;
         }
-        if (membership !== this.membership) {
-            this.membership = membership;
+        if (membership !== this._membership) {
+            this._membership = membership;
+            changed = true;
         }
         // state comes before timeline
         if (roomResponse.state) {
-            for (const e of roomResponse.state.events) {
-                this._processEvent(e);
-            }
+            changed = roomResponse.state.events.reduce((changed, e) => {
+                return this._processEvent(e) || changed;
+            }, changed);
         }
         if (roomResponse.timeline) {
-            for(const e of roomResponse.timeline.events) {
-                this._processEvent(e);
-            }
+            changed = roomResponse.timeline.events.reduce((changed, e) => {
+                return this._processEvent(e) || changed;
+            }, changed);
         }
+
+        return changed;
     }
 
     _processEvent(event) {
         if (event.type === "m.room.encryption") {
             if (!this.isEncrypted) {
                 this.isEncrypted = true;
+                return true;
             }
-        } else if (event.type === "m.room.name") {
+        }
+        if (event.type === "m.room.name") {
             const newName = event.content && event.content.name;
             if (newName !== this.name) {
                 this.name = newName;
+                return true;
             }
         } else if (event.type === "m.room.member") {
-            this._processMembership(event);
+            return this._processMembership(event);
         } else if (event.type === "m.room.message") {
             const content = event.content;
             const body = content && content.body;
             const msgtype = content && content.msgtype;
             if (msgtype === "m.text") {
                 this.lastMessageBody = body;
+                return true;
             }
         } else if (event.type === "m.room.canonical_alias") {
             const content = event.content;
             this.canonicalAlias = content.alias;
+            return true;
         } else if (event.type === "m.room.aliases") {
             const content = event.content;
             this.aliases = content.aliases;
+            return true;
         }
+        return false;
     }
 
     _processMembership(event) {
+        let changed = false;
         const prevMembership = event.prev_content && event.prev_content.membership;
         if (!event.content) {
-            return;
+            return changed;
         }
         const content = event.content;
         const membership = content.membership;
@@ -111,7 +139,13 @@ class SummaryData extends ChangeSet {
                 case "invite": this.inviteCount += 1; break;
                 case "join": this.joinCount += 1; break;
             }
+            changed = true;
         }
+        // if (membership === "join" && content.name) {
+        //  // TODO: avatar_url
+        //  changed = this._members.applyMember(content.name, content.state_key) || changed;
+        // }
+        return changed;
     }
 
     _updateSummary(summary) {
@@ -133,7 +167,7 @@ class SummaryData extends ChangeSet {
 
 export default class RoomSummary {
 	constructor(roomId) {
-        this._data = SummaryData.create(roomId);
+        this._data = new SummaryData(roomId);
 	}
 
 	get name() {
@@ -164,6 +198,8 @@ export default class RoomSummary {
 		return this._data.joinCount;
 	}
 
+    // writeSync is not a good name, this isn't writing at all!
+    // processSync?
 	writeSync(roomResponse, membership, txn) {
         // write changes to a clone that we only 
         // reassign back once the transaction was succesfully committed
@@ -191,4 +227,15 @@ export default class RoomSummary {
             this._data[key] = value;
         }
 	}
+}
+
+export function tests() {
+    return {
+        "membership trigger change": function(assert) {
+            const summary = new RoomSummary("id");
+            const changes = summary.writeSync({}, "join");
+            assert(changes);
+            assert(changes.changed);
+        }
+    }
 }
