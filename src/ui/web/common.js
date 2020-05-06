abc99c2f0b6542b00722dc8b6d2d16c736344ec7
Bruno Windels <brunow@matrix.org>
2020-05-06 23:31:22 +0200

    fix svg error in spinner

--- src/ui/web/common.js
+++ src/ui/web/common.js
@@ -1,5 +1,5 @@
 export function spinner(t, extraClasses = undefined) {
-    return t.svg({className: Object.assign({"spinner": true}, extraClasses), viewBox:"0 0 100% 100%"}, 
+    return t.svg({className: Object.assign({"spinner": true}, extraClasses), viewBox:"0 0 100 100"}, 
         t.circle({cx:"50%", cy:"50%", r:"45%", pathLength:"100"})
     );
 }
