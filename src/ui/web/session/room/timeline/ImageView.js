1ec25434671485e13dcbe09ef9963a65020a8c72
Bruno Windels <brunow@matrix.org>
2020-05-10 11:56:05 +0200

    fix image aspect ratio when scaling, again
    also when image hasn't loaded yet to prevent scroll position jumps

--- src/ui/web/session/room/timeline/ImageView.js
+++ src/ui/web/session/room/timeline/ImageView.js
@@ -2,12 +2,26 @@ import {TemplateView} from "../../../general/TemplateView.js";
 
 export class ImageView extends TemplateView {
     render(t, vm) {
+        // replace with css aspect-ratio once supported
+        const heightRatioPercent = (vm.thumbnailHeight / vm.thumbnailWidth) * 100;
+        const image = t.img({
+            src: vm.thumbnailUrl,
+            width: vm.thumbnailWidth,
+            height: vm.thumbnailHeight,
+            loading: "lazy",
+            alt: vm.label,
+        });
+        const linkContainer = t.a({
+            href: vm.url,
+            target: "_blank",
+            style: `padding-top: ${heightRatioPercent}%; width: ${vm.thumbnailWidth}px;`
+        }, image);
+
         return t.li(
             {className: {"TextMessageView": true, own: vm.isOwn, pending: vm.isPending}},
             t.div({className: "message-container"}, [
                 t.div({className: "sender"}, vm => vm.isContinuation ? "" : vm.sender),
-                t.div(t.a({href: vm.url, target: "_blank"},
-                    t.img({src: vm.thumbnailUrl, width: vm.thumbnailWidth, height: vm.thumbnailHeight, loading: "lazy", alt: vm.label}))),
+                t.div(linkContainer),
                 t.p(t.time(vm.date + " " + vm.time)),
             ])
         );
