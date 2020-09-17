3cebd17cbed4fa31a47bc940d5ec41bc95a2152e
Bruno Windels <brunow@matrix.org>
2020-09-17 15:17:46 +0200

    adjust paths

--- src/matrix/ssss/SecretStorage.js
+++ src/matrix/ssss/SecretStorage.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {base64} from "../../../utils/base-encoding.js";
+import {base64} from "../../utils/base-encoding.js";
 
 export class SecretStorage {
     constructor({key, storage, cryptoDriver}) {
