ceac6008bc9aa62bcbe824156f36d9fe21327d85
Bruno Windels <brunow@matrix.org>
2020-09-17 18:55:09 +0200

    fix import

--- src/legacy-extras.js
+++ src/legacy-extras.js
@@ -1,5 +1,5 @@
 import aesjs from "../lib/aes-js/index.js";
-import hkdf from "./utils/crypto/hkdf.js";
+import {hkdf} from "./utils/crypto/hkdf.js";
 
 // these are run-time dependencies that are only needed for the legacy bundle.
 // they are exported here and passed into main to make them available to the app.
