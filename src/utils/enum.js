3aead4eae2f89fe8ae2bc550ed277f5ec166eace
Bruno Windels <brunow@matrix.org>
2020-09-18 13:08:35 +0200

    createEnum does not take an array

--- src/utils/enum.js
+++ src/utils/enum.js
@@ -17,6 +17,9 @@ limitations under the License.
 export function createEnum(...values) {
     const obj = {};
     for (const value of values) {
+        if (typeof value !== "string") {
+            throw new Error("Invalid enum value name" + value?.toString());
+        }
         obj[value] = value;
     }
     return Object.freeze(obj);
