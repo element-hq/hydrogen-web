e175e9ac8baa11287a308eeff57f24a22af96f9d
Bruno Windels <brunow@matrix.org>
2020-09-25 10:45:34 +0200

    cleanup

--- src/matrix/net/HomeServerApi.js
+++ src/matrix/net/HomeServerApi.js
@@ -75,7 +75,7 @@ export class HomeServerApi {
             method,
             headers,
             body: bodyString,
-            timeout: options && options.timeout
+            timeout: options?.timeout
         });
 
         const wrapper = new RequestWrapper(method, url, requestResult);
