3ab68ef4381f106a483a2b48ab8776b10e1195e3
Bruno Windels <brunow@matrix.org>
2020-10-09 17:01:54 +0200

    throw when something tracked is not disposable, fail early

--- src/utils/Disposables.js
+++ src/utils/Disposables.js
@@ -22,6 +22,10 @@ function disposeValue(value) {
     }
 }
 
+function isDisposable(value) {
+    return value && (typeof value === "function" || typeof value.dispose === "function");
+}
+
 export class Disposables {
     constructor() {
         this._disposables = [];
@@ -30,6 +34,9 @@ export class Disposables {
     track(disposable) {
         if (this.isDisposed) {
             throw new Error("Already disposed, check isDisposed after await if needed");
+        }
+        if (!isDisposable(disposable)) {
+            throw new Error("Not a disposable");
         }
         this._disposables.push(disposable);
         return disposable;
