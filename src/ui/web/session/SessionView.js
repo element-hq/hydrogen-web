cf99f55f3d142a31b231b03cf9e51a3417394ecb
Bruno Windels <brunow@matrix.org>
2020-10-14 14:20:53 +0200

    fix typo making the room list not shown on the mobile layout

--- src/ui/web/session/SessionView.js
+++ src/ui/web/session/SessionView.js
@@ -26,7 +26,7 @@ export class SessionView extends TemplateView {
         return t.div({
             className: {
                 "SessionView": true,
-                "room-shown": vm => vm.selectionId !== "placeholder"
+                "room-shown": vm => vm.activeSection !== "placeholder"
             },
         }, [
             t.view(new SessionStatusView(vm.sessionStatusViewModel)),
