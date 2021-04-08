bdc8451c4a085b8f34c077be91fd60be636adebd
Bruno Windels <brunow@element.io>
2021-04-08 18:39:52 +0200

    better logging

--- src/matrix/room/sending/SendQueue.js
+++ src/matrix/room/sending/SendQueue.js
@@ -118,7 +118,7 @@ export class SendQueue {
             }
             if (idx !== -1) {
                 const pendingEvent = this._pendingEvents.get(idx);
-                parentLog.log({l: "removeRemoteEcho", id: pendingEvent.remoteId});
+                parentLog.log({l: "removeRemoteEcho", queueIndex: pendingEvent.queueIndex, remoteId: event.event_id, txnId});
                 txn.pendingEvents.remove(pendingEvent.roomId, pendingEvent.queueIndex);
                 removed.push(pendingEvent);
             }
