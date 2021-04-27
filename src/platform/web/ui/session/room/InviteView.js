c47b27428b8a6acd17efcca8561d980542dd5b3f
Bruno Windels <brunow@element.io>
2021-04-27 15:10:12 +0200

    implement invite view + styling

--- src/platform/web/ui/session/room/InviteView.js
+++ src/platform/web/ui/session/room/InviteView.js
@@ -16,27 +16,57 @@ limitations under the License.
 */
 
 import {TemplateView} from "../../general/TemplateView.js";
-// import {TimelineList} from "./TimelineList.js";
-// import {TimelineLoadingView} from "./TimelineLoadingView.js";
-// import {MessageComposer} from "./MessageComposer.js";
 import {renderStaticAvatar} from "../../avatar.js";
 
 export class InviteView extends TemplateView {
     render(t, vm) {
+        let inviteNodes = [];
+        if (vm.isDirectMessage) {
+            inviteNodes.push(renderStaticAvatar(vm, 128, "InviteView_dmAvatar"));
+        }
+        let inviterNodes;
+        if (vm.isDirectMessage) {
+            inviterNodes = [t.strong(vm.name), ` (${vm.inviter?.id}) wants to chat with you.`];
+        } else if (vm.inviter) {
+            inviterNodes = [renderStaticAvatar(vm.inviter, 24), t.strong(vm.inviter.name), ` (${vm.inviter.id}) invited you.`];
+        } else {
+            inviterNodes = `You were invited to join.`;
+        }
+        inviteNodes.push(t.p({className: "InviteView_inviter"}, inviterNodes));
+        if (!vm.isDirectMessage) {
+            inviteNodes.push(t.div({className: "InviteView_roomProfile"}, [
+                renderStaticAvatar(vm, 64, "InviteView_roomAvatar"),
+                t.h3(vm.name),
+                t.p({className: "InviteView_roomDescription"}, vm.roomDescription)
+            ]));
+        }
+
         return t.main({className: "InviteView middle"}, [
-            t.div({className: "TimelinePanel"}, [
-                t.div({className: "RoomHeader middle-header"}, [
-                    t.a({className: "button-utility close-middle", href: vm.closeUrl, title: vm.i18n`Close invite`}),
-                    renderStaticAvatar(vm, 32),
-                    t.div({className: "room-description"}, [
-                        t.h2(vm => vm.name),
-                    ]),
+            t.div({className: "RoomHeader middle-header"}, [
+                t.a({className: "button-utility close-middle", href: vm.closeUrl, title: vm.i18n`Close invite`}),
+                renderStaticAvatar(vm, 32),
+                t.div({className: "room-description"}, [
+                    t.h2(vm => vm.name),
                 ]),
-                t.div({className: "RoomView_error"}, vm => vm.error),
-                t.div([
-                    t.p(`You were invited into this room!`),
-                    t.p(t.button({onClick: () => vm.accept()}, vm.i18n`Accept`)),
-                    t.p(t.button({onClick: () => vm.reject()}, vm.i18n`Reject`)),
+            ]),
+            t.if(vm => vm.error, t => t.div({className: "RoomView_error"}, vm => vm.error)),
+            t.div({className: "InviteView_body"}, [
+                t.div({className: "InviteView_invite"}, [
+                    ...inviteNodes,
+                    t.div({className: "InviteView_buttonRow"},
+                        t.button({
+                            className: "button-action primary",
+                            disabled: vm => vm.busy,
+                            onClick: () => vm.accept()
+                        }, vm.i18n`Accept`)
+                    ),
+                    t.div({className: "InviteView_buttonRow"},
+                        t.button({
+                            className: "button-action primary destructive",
+                            disabled: vm => vm.busy,
+                            onClick: () => vm.reject()
+                        }, vm.i18n`Reject`)
+                    ),
                 ])
             ])
         ]);
