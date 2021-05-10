787d438a746a313da65150d28437040d20c1c100
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-10 21:30:11 +0530

    Refactored Linkifier class into single function
    Modified design so that linkify interacts with MessageBodyBuilder
    through callbacks.
    
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

---
+++ src/domain/session/room/timeline/linkify.js
@@ -0,0 +1,12 @@
+export function linkify(text, callback) {
+    const regex = /(?:https|http|ftp):\/\/[a-zA-Z0-9:.\[\]#-]+(?:\/[^\s]*[^\s.,?!]|[^\s\u{80}-\u{10ffff}.,?!])/gui
+    const matches = text.matchAll(regex);
+    let curr = 0;
+    for (let match of matches) {
+        callback(match[0], true);
+        callback(text.slice(curr, match.index), false);
+        const len = match[0].length;
+        curr = match.index + len;
+    }
+    callback(text.slice(curr), false);
+}
