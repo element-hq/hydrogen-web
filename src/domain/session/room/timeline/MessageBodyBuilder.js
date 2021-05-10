55f2d79d077a83eeecb0bc34ab16cc222d5df57d
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-10 21:37:27 +0530

    Move newline handling to MessageBodyBuilder
    - Also add a insert method to reflect new design.
    
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/domain/session/room/timeline/MessageBodyBuilder.js
+++ src/domain/session/room/timeline/MessageBodyBuilder.js
@@ -1,7 +1,26 @@
+import { linkify } from "./linkify.js";
+
 export class MessageBodyBuilder {
 
     constructor(message = []) {
         this._root = message;
+    }
+
+    fromText(text) {
+        const components = text.split("\n");
+        components.slice(0, -1).forEach(t => {
+            linkify(t, this.insert.bind(this));
+        });
+        const [last] = components.slice(-1);
+        linkify(last, this.insert.bind(this));
+    }
+
+    insert(text, isLink) {
+        if (!text.length) return;
+        if (isLink)
+            this.insertLink(text, text);
+        else
+            this.insertText(text);
     }
 
     insertText(text) {
