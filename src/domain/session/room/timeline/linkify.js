027f6020e76b1de94963082c58d50d9e2c6c71b2
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-11 22:15:36 +0530

    Modified regex to allow unicode in fragment
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/domain/session/room/timeline/linkify.js
+++ src/domain/session/room/timeline/linkify.js
@@ -1,4 +1,4 @@
-const regex = /(?:https|http|ftp):\/\/[a-zA-Z0-9:.\[\]#-]+(?:\/[^\s]*[^\s.,?!]|[^\s\u{80}-\u{10ffff}.,?!])/gui
+const regex = /(?:https|http|ftp):\/\/[a-zA-Z0-9:.\[\]#-]+(?:[\/#][^\s]*[^\s.,?!]|[^\s\u{80}-\u{10ffff}.,?!])/gui
 
 export function linkify(text, callback) {
     const matches = text.matchAll(regex);
