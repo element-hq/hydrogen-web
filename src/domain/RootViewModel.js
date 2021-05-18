011eeb21d9ebbc65e74681d4b79c6342e28814bc
Johannes Marbach <n0-0ne+github@mailbox.org>
2021-05-18 21:16:52 +0200

    Restore theme at the beginning of load

--- src/domain/RootViewModel.js
+++ src/domain/RootViewModel.js
@@ -33,10 +33,10 @@ export class RootViewModel extends ViewModel {
     }
 
     async load() {
+        this.platform.themeManager.setTheme(await this.platform.settingsStorage.getString("theme"));
         this.track(this.navigation.observe("login").subscribe(() => this._applyNavigation()));
         this.track(this.navigation.observe("session").subscribe(() => this._applyNavigation()));
         this._applyNavigation(true);
-        this.platform.themeManager.setTheme(await this.platform.settingsStorage.getString("theme"));
     }
 
     async _applyNavigation(shouldRestoreLastUrl) {
