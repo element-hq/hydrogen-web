d502a7f911adb9d378deb7d2576b5a9784576c05
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-25 16:09:47 +0530

    Make display name bold
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/platform/web/ui/session/rightpanel/RoomInfoView.js
+++ src/platform/web/ui/session/rightpanel/RoomInfoView.js
@@ -6,7 +6,7 @@ export class RoomInfoView extends TemplateView {
     render(t, vm) {
         return t.div({ className: "RoomInfo" }, [
             t.div({ className: "RoomAvatar" }, [t.view(new AvatarView(vm, 128))]),
-            t.div({ className: "RoomName" }, [text(vm.name)]),
+            t.div({ className: "RoomName" }, [t.h2(vm.name)]),
             t.div({ className: "RoomId" }, [text(vm.roomId)]),
             t.div({ className: "RoomMemberCount" }, [text(vm.memberCount)]),
             t.div({ className: "RoomEncryption" }, [vm.isEncrypted ? "Encrypted" : "Not Encrypted"])
