fd54539e1c46b4362331f486750a5626681e3898
Bruno Windels <bruno@windels.cloud>
2021-06-17 09:41:10 +0200

    clarify comment

--- src/domain/session/room/timeline/ReactionsViewModel.js
+++ src/domain/session/room/timeline/ReactionsViewModel.js
@@ -128,7 +128,7 @@ class ReactionViewModel {
     }
 
     _compare(other) {
-        // the comparator is also used to test for equality, if the comparison returns 0
+        // the comparator is also used to test for equality by sortValues, if the comparison returns 0
         // given that the firstTimestamp isn't set anymore when the last reaction is removed,
         // the remove event wouldn't be able to find the correct index anymore. So special case equality.
         if (other === this) {
