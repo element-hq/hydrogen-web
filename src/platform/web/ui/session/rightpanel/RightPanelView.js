abd2c19bf29a63ecb188538e60e4e50b6085a478
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-06-17 15:18:05 +0530

    Remove unused param
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/platform/web/ui/session/rightpanel/RightPanelView.js
+++ src/platform/web/ui/session/rightpanel/RightPanelView.js
@@ -3,7 +3,7 @@ import {RoomDetailsView} from "./RoomDetailsView.js";
 import {MemberListView} from "./MemberListView.js";
 
 export class RightPanelView extends TemplateView {
-    render(t, vm) {
+    render(t) {
         return t.div({className: "RightPanelView"}, 
             [
                 t.mapView(vm => vm.roomDetailsViewModel, roomDetailsViewModel => roomDetailsViewModel ? new RoomDetailsView(roomDetailsViewModel) : null),
