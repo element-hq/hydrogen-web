7e72d57be7942273bb1fd819cb13636ff4e7d8db
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-06-17 16:45:36 +0530

    Make list scrollable
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/platform/web/ui/session/rightpanel/MemberListView.js
+++ src/platform/web/ui/session/rightpanel/MemberListView.js
@@ -4,6 +4,6 @@ import {MemberTileView} from "./MemberTileView.js";
 
 export class MemberListView extends TemplateView {
     render(t, vm) {
-        return t.view(new ListView({list: vm.memberTileViewModels}, tileViewModel => new MemberTileView(tileViewModel)));
+        return t.view(new ListView({list: vm.memberTileViewModels, className:"MemberListView"}, tileViewModel => new MemberTileView(tileViewModel)));
     }
 }
