ddb7a16fb8a22e21ba614f9e891bc0486156f115
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-06-18 13:12:20 +0530

    Make member private
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/domain/session/rightpanel/MemberTileViewModel.js
+++ src/domain/session/rightpanel/MemberTileViewModel.js
@@ -3,10 +3,14 @@ import {ViewModel} from "../../ViewModel.js";
 export class MemberTileViewModel extends ViewModel {
     constructor(options) {
         super(options);
-        this.member = options.member;
+        this._member = options.member;
     }
 
     get displayName() {
-        return this.member.displayName;
+        return this._member.displayName;
+    }
+
+    get userId() {
+        return this._member.userId;
     }
 }
