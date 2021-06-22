404129f60eb18f68243f14f7a9efa04eda30b9bb
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-06-22 17:33:31 +0530

    Settle on MappedMap
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/domain/session/rightpanel/MemberListViewModel.js
+++ src/domain/session/rightpanel/MemberListViewModel.js
@@ -1,13 +1,12 @@
 import {ViewModel} from "../../ViewModel.js";
 import {MemberTileViewModel} from "./MemberTileViewModel.js";
 import {createMemberComparator} from "./comparator.js";
-import {MappedList} from "../../../observable/list/MappedList.js";
 
 export class MemberListViewModel extends ViewModel {
     constructor(options) {
         super(options);
-        this.memberTileViewModels = this._mapTileViewModels(this._filterJoinedMembers(options.members)
-                                                                .sortValues(createMemberComparator(options.powerLevels)));
+        this.memberTileViewModels = this._mapTileViewModels(this._filterJoinedMembers(options.members))
+                                        .sortValues(createMemberComparator(options.powerLevels));
     }
 
     _filterJoinedMembers(members) {
@@ -15,10 +14,10 @@ export class MemberListViewModel extends ViewModel {
     }
 
     _mapTileViewModels(members) {
-        const mapper = (member) => {
-            return new MemberTileViewModel(this.childOptions({member}));
+        const mapper = (member, emitUpdate) => {
+            return new MemberTileViewModel(this.childOptions({member, emitUpdate}));
         }
         const updater = (vm, params, newMember) => vm.updateFrom(newMember);
-        return new MappedList(members, mapper, updater);
+        return members.mapValues(mapper, updater);
     }
 }
