b62473ae79cdc1731ddd3228c90f1b2b1c571461
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-06-22 15:51:18 +0530

    Add update method
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/domain/session/rightpanel/MemberTileViewModel.js
+++ src/domain/session/rightpanel/MemberTileViewModel.js
@@ -3,7 +3,8 @@ import {ViewModel} from "../../ViewModel.js";
 export class MemberTileViewModel extends ViewModel {
     constructor(options) {
         super(options);
-        this._member = options.member;
+        this._member = this._options.member;
+        this._shouldDisambiguate = options.shouldDisambiguate;
     }
 
     get name() {
@@ -13,4 +14,9 @@ export class MemberTileViewModel extends ViewModel {
     get userId() {
         return this._member.userId;
     }
+
+    updateFrom(newMember) {
+        this._member = newMember;
+    }
+
 }
