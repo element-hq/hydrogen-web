35f6043d06e2aedc3c91721cae097fae1fd7ad7b
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-07-03 21:18:28 +0530

    Support slice
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/observable/list/SortedMapList.js
+++ src/observable/list/SortedMapList.js
@@ -119,6 +119,10 @@ export class SortedMapList extends BaseObservableList {
         return this._sourceMap.size;
     }
 
+    slice(start, end) {
+        return this._sortedPairs.slice(start, end);
+    }
+
     [Symbol.iterator]() {
         const it = this._sortedPairs.values();
         return {
