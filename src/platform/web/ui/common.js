7c53c85aa5659a45eca488a6091bd577ce154769
Richard Lewis <richard@smetco.co.uk>
2021-07-06 20:05:56 +0100

    Formatting

--- src/platform/web/ui/common.js
+++ src/platform/web/ui/common.js
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const container = document.querySelector(".hydrogen");
 
 export function spinner(t, extraClasses = undefined) {
+    const container = document.querySelector(".hydrogen");
     if (container.classList.contains("legacy")) {
         return t.div({className: "spinner"}, [
             t.div(),
