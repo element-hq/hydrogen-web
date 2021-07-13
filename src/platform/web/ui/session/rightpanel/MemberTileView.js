f506cf615bd98bb63f016674ed26f522f4368330
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-07-13 17:39:17 +0530

    div --> li
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/platform/web/ui/session/rightpanel/MemberTileView.js
+++ src/platform/web/ui/session/rightpanel/MemberTileView.js
@@ -3,7 +3,7 @@ import {AvatarView} from "../../avatar.js";
 
 export class MemberTileView extends TemplateView {
     render(t, vm) {
-        return t.div({ className: "MemberTileView" }, [
+        return t.li({ className: "MemberTileView" }, [
             t.view(new AvatarView(vm, 32)),
             t.div({ className: "MemberTileView_name" }, (vm) => vm.name),
         ]);
