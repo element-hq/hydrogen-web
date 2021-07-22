16887c3c33bf5c66aba7229379b1e1795bdb01ae
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-07-22 19:59:58 +0530

    Better formatting
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/platform/web/ui/session/rightpanel/MemberTileView.js
+++ src/platform/web/ui/session/rightpanel/MemberTileView.js
@@ -3,7 +3,9 @@ import {AvatarView} from "../../AvatarView.js";
 
 export class MemberTileView extends TemplateView {
     render(t, vm) {
-        return t.li({ className: "MemberTileView" }, t.a({href: vm.detailsUrl}, [
+        return t.li({ className: "MemberTileView" },
+            t.a({ href: vm.detailsUrl },
+            [
                 t.view(new AvatarView(vm, 32)),
                 t.div({ className: "MemberTileView_name" }, (vm) => vm.name),
             ])
