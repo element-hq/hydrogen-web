4784bcc59085ccf5eb8247eb8487b95b81fa65d4
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-07-22 19:58:04 +0530

    Make the tiles link to details
    Signed-off-by: RMidhunSuresh <rmidhunsuresh@gmail.com>

--- src/platform/web/ui/session/rightpanel/MemberTileView.js
+++ src/platform/web/ui/session/rightpanel/MemberTileView.js
@@ -3,9 +3,10 @@ import {AvatarView} from "../../AvatarView.js";
 
 export class MemberTileView extends TemplateView {
     render(t, vm) {
-        return t.li({ className: "MemberTileView" }, [
-            t.view(new AvatarView(vm, 32)),
-            t.div({ className: "MemberTileView_name" }, (vm) => vm.name),
-        ]);
+        return t.li({ className: "MemberTileView" }, t.a({href: vm.detailsUrl}, [
+                t.view(new AvatarView(vm, 32)),
+                t.div({ className: "MemberTileView_name" }, (vm) => vm.name),
+            ])
+        );
     }
 }
