177dfd339a29c195f40dca469e009c1ad6d7bb59
Danila Fedorin <danila.fedorin@gmail.com>
2021-08-06 12:17:03 -0700

    Add a basic snowpack configuration

---
+++ snowpack.config.js
@@ -0,0 +1,29 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    "src": "/src",
+    "public": "/",
+    "lib": {url: "/lib", static: true },
+    /* ... */
+  },
+  exclude: [
+    /* Avoid scanning scripts which use dev-dependencies and pull in babel, rollup, etc. */
+    '**/node_modules/**/*', '**/scripts/**', '**/target/**', '**/prototypes/**', '**/src/matrix/storage/memory/**'
+  ],
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    open: "none",
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
