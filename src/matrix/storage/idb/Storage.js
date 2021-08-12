5f3c9cda975e6ea6e6a62ab5556472c3e4261a02
Danila Fedorin <danila.fedorin@gmail.com>
2021-08-12 12:38:44 -0700

    Migrate Transaction to TypeScript

--- src/matrix/storage/idb/Storage.js
+++ src/matrix/storage/idb/Storage.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {Transaction} from "./Transaction.js";
+import {Transaction} from "./Transaction";
 import { STORE_NAMES, StoreNames, StorageError } from "../common";
 import { reqAsPromise } from "./utils";
 
