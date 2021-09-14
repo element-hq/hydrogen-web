f8117b6f986d662fc9961b84874aec861780beae
Danila Fedorin <danila.fedorin@gmail.com>
2021-09-14 11:18:24 -0700

    Lift transaction property to QueryTarget

--- src/matrix/storage/idb/Store.ts
+++ src/matrix/storage/idb/Store.ts
@@ -128,16 +128,8 @@ class QueryTargetWrapper<T> {
 }
 
 export class Store<T> extends QueryTarget<T> {
-    private _transaction: Transaction;
-
     constructor(idbStore: IDBObjectStore, transaction: Transaction) {
-        super(new QueryTargetWrapper<T>(idbStore), transaction.idbFactory, transaction.IDBKeyRange);
-        this._transaction = transaction;
-    }
-
-    get IDBKeyRange() {
-        // @ts-ignore
-        return this._transaction.IDBKeyRange;
+        super(new QueryTargetWrapper<T>(idbStore), transaction);
     }
 
     get _idbStore(): QueryTargetWrapper<T> {
@@ -145,7 +137,7 @@ export class Store<T> extends QueryTarget<T> {
     }
 
     index(indexName: string): QueryTarget<T> {
-        return new QueryTarget<T>(new QueryTargetWrapper<T>(this._idbStore.index(indexName)), this._idbFactory, this._IDBKeyRange);
+        return new QueryTarget<T>(new QueryTargetWrapper<T>(this._idbStore.index(indexName)), this._transaction);
     }
 
     put(value: T): void {
