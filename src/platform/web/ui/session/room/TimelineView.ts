1c06e36c1c13a29d766e7bd9c0e41a94196f18a5
Bruno Windels <bruno@windels.cloud>
2021-10-01 10:07:17 +0200

    add override keyword

--- src/platform/web/ui/session/room/TimelineView.ts
+++ src/platform/web/ui/session/room/TimelineView.ts
@@ -211,12 +211,12 @@ class TilesListView extends ListView<SimpleTile, TileView> {
         this.onChanged = onChanged;
     }
 
-    onReset() {
+    override onReset() {
         super.onReset();
         this.onChanged();
     }
 
-    onUpdate(index: number, value: SimpleTile, param: any) {
+    override onUpdate(index: number, value: SimpleTile, param: any) {
         if (param === "shape") {
             const ExpectedClass = viewClassForEntry(value);
             const child = this.getChildInstanceByIndex(index);
@@ -232,17 +232,17 @@ class TilesListView extends ListView<SimpleTile, TileView> {
         this.onChanged();
     }
 
-    onAdd(idx: number, value: SimpleTile) {
+    override onAdd(idx: number, value: SimpleTile) {
         super.onAdd(idx, value);
         this.onChanged();
     }
 
-    onRemove(idx: number, value: SimpleTile) {
+    override onRemove(idx: number, value: SimpleTile) {
         super.onRemove(idx, value);
         this.onChanged();
     }
 
-    onMove(fromIdx: number, toIdx: number, value: SimpleTile) {
+    override onMove(fromIdx: number, toIdx: number, value: SimpleTile) {
         super.onMove(fromIdx, toIdx, value);
         this.onChanged();
     }
