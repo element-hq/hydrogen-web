6d8ec69a4d0218b61f85f4ac118d0b8aac86dd70
Bruno Windels <bruno@windels.cloud>
2021-10-22 18:01:17 +0200

    fix lint

--- src/matrix/Session.js
+++ src/matrix/Session.js
@@ -27,6 +27,7 @@ import {Account as E2EEAccount} from "./e2ee/Account.js";
 import {Decryption as OlmDecryption} from "./e2ee/olm/Decryption.js";
 import {Encryption as OlmEncryption} from "./e2ee/olm/Encryption.js";
 import {Decryption as MegOlmDecryption} from "./e2ee/megolm/Decryption";
+import {KeyLoader as MegOlmKeyLoader} from "./e2ee/megolm/decryption/KeyLoader";
 import {SessionBackup} from "./e2ee/megolm/SessionBackup.js";
 import {Encryption as MegOlmEncryption} from "./e2ee/megolm/Encryption.js";
 import {MEGOLM_ALGORITHM} from "./e2ee/common.js";
@@ -137,7 +138,7 @@ export class Session {
             now: this._platform.clock.now,
             ownDeviceId: this._sessionInfo.deviceId,
         });
-        const keyLoader = new KeyLoader(this._olm, PICKLE_KEY, 20);
+        const keyLoader = new MegOlmKeyLoader(this._olm, PICKLE_KEY, 20);
         this._megolmDecryption = new MegOlmDecryption(keyLoader, this._olmWorker);
         this._deviceMessageHandler.enableEncryption({olmDecryption, megolmDecryption: this._megolmDecryption});
     }
