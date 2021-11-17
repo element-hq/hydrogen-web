fe6090183f8b76149475a9e744e97646dfc38c11
RMidhunSuresh <hi@midhun.dev>
2021-11-17 18:18:14 +0530

    Rename Platform.js to Platform.ts

--- src/platform/web/LegacyPlatform.js
+++ src/platform/web/LegacyPlatform.js
@@ -16,7 +16,7 @@ limitations under the License.
 
 import aesjs from "../../../lib/aes-js/index.js";
 import {hkdf} from "../../utils/crypto/hkdf.js";
-import {Platform as ModernPlatform} from "./Platform.js";
+import {Platform as ModernPlatform} from "./Platform";
 
 export function Platform(container, paths) {
     return new ModernPlatform(container, paths, {aesjs, hkdf});
