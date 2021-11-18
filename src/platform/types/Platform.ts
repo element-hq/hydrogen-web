2a3b13ecce4d40f5e0fd98d59fb0645ad6a541ab
RMidhunSuresh <hi@midhun.dev>
2021-11-18 17:06:20 +0530

    Add request type

---
+++ src/platform/types/Platform.ts
@@ -0,0 +1,37 @@
+/*
+Copyright 2021 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import type {RequestResult} from "../web/dom/request/fetch.js";
+import type {BlobHandle} from "../web/dom/BlobHandle.js";
+
+//ts-todo: This should go somewhere else?
+interface BlobBody {
+    mimeType: string;
+    body: BlobHandle;
+    length: number;
+}
+
+interface IRequestOptions {
+    uploadProgress?: (loadedBytes: number) => void;
+    timeout?: number;
+    body?: string | BlobBody;
+    headers?: { [key: string]: number | string };
+    cache?: boolean;
+    method: string;
+    format: string;
+}
+
+export type Request = (url: string, options: IRequestOptions) => RequestResult;
