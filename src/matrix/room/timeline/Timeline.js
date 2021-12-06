65f957f023e9de9499f69f8f000917ede41118fb
RMidhunSuresh <hi@midhun.dev>
2021-12-06 11:36:45 +0530

    WIP

--- src/matrix/room/timeline/Timeline.js
+++ src/matrix/room/timeline/Timeline.js
@@ -224,7 +224,7 @@ export class Timeline {
     }
 
     /** @package */
-    replaceEntries(entries) {
+     replaceEntries(entries) {
         this._addLocalRelationsToNewRemoteEntries(entries);
         for (const entry of entries) {
             try {
@@ -366,7 +366,7 @@ export class Timeline {
         }
         return eventEntry;
     }
-    
+
     // tries to prepend `amount` entries to the `entries` list.
     /**
      * [loadAtTop description]
