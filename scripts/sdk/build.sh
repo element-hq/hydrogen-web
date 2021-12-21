163dae647b0531c01ed6396cb7ac19a6fb72595d
Bruno Windels <bruno@windels.cloud>
2021-12-21 12:24:33 +0100

    move output of both lib and asset build around for coherent package

--- scripts/sdk/build.sh
+++ scripts/sdk/build.sh
@@ -1,4 +1,17 @@
+rm -rf target
 yarn run vite build -c vite.sdk-assets-config.js
 yarn run vite build -c vite.sdk-lib-config.js
 yarn tsc -p tsconfig-declaration.json
 ./scripts/sdk/create-manifest.js ./target/package.json
+pushd target
+pushd asset-build/assets
+mv main.*.js ../../main.js
+mv index.*.css ../../style.css
+mv download-sandbox.*.html ../../download-sandbox.html
+rm *.js *.wasm
+mv ./* ../../
+popd
+rm -rf asset-build
+mv lib-build/* .
+rm -rf lib-build
+popd
