412db33c365279dacbe49e934fbb1a6995c8e104
Bruno Windels <bruno@windels.cloud>
2022-01-14 19:18:12 +0100

    click here labels are so nineties

--- src/platform/web/ui/session/room/timeline/LocationView.js
+++ src/platform/web/ui/session/room/timeline/LocationView.js
@@ -20,7 +20,7 @@ export class LocationView extends BaseMessageView {
     renderMessageBody(t, vm) {
         return t.p({className: "Timeline_messageBody statusMessage"}, [
             t.span(vm.label),
-            t.a({className: "Timeline_locationLink", href: vm.mapsLink, target: "_blank", rel: "noopener"}, vm.i18n`Click to open in maps`),
+            t.a({className: "Timeline_locationLink", href: vm.mapsLink, target: "_blank", rel: "noopener"}, vm.i18n`Open in maps`),
             t.time(vm.date + " " + vm.time)
         ]);
     }
