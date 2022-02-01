550a560f40330468a7ce05a59e27c295702f842b
RMidhunSuresh <hi@midhun.dev>
2022-02-01 17:54:08 +0530

    Remove space

--- src/matrix/registration/Registration.ts
+++ src/matrix/registration/Registration.ts
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 import type {HomeServerApi} from "../net/HomeServerApi";
-import { registrationStageFromType } from "./registrationStageFromType";
+import {registrationStageFromType} from "./registrationStageFromType";
 import type {BaseRegistrationStage} from "./stages/BaseRegistrationStage";
 
 export type RegistrationParameters = {
