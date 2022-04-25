22df062bbb4723f8f028fdb89827f3e8d3e10d23
Bruno Windels <274386+bwindels@users.noreply.github.com>
2022-04-25 14:05:02 +0200

    fix observable typescript errors

--- src/matrix/room/timeline/Timeline.js
+++ src/matrix/room/timeline/Timeline.js
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {SortedArray, AsyncMappedList, ConcatList, ObservableArray} from "../../../observable/index.js";
+import {SortedArray, AsyncMappedList, ConcatList, ObservableArray} from "../../../observable/index";
 import {Disposables} from "../../../utils/Disposables";
 import {Direction} from "./Direction";
 import {TimelineReader} from "./persistence/TimelineReader.js";
