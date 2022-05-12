e63440527a99172b31b684e9f98551e171674342
RMidhunSuresh <hi@midhun.dev>
2022-05-12 13:43:19 +0530

    Move condition to binding

--- src/platform/web/ui/session/settings/SettingsView.js
+++ src/platform/web/ui/session/settings/SettingsView.js
@@ -97,8 +97,8 @@ export class SettingsView extends TemplateView {
         settingNodes.push(
             t.h3("Preferences"),
             row(t, vm.i18n`Scale down images when sending`, this._imageCompressionRange(t, vm)),
-            t.if(vm => vm.activeTheme, (t, vm) => {
-                return !import.meta.env.DEV? row(t, vm.i18n`Use the following theme`, this._themeOptions(t, vm)): null;
+            t.if(vm => !import.meta.env.DEV && vm.activeTheme, (t, vm) => {
+                return row(t, vm.i18n`Use the following theme`, this._themeOptions(t, vm));
             }),
         );
         settingNodes.push(
