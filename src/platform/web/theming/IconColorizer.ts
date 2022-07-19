83b5d3b68e6ee7e497bbdf0e1c407f2be03716d8
RMidhunSuresh <hi@midhun.dev>
2022-07-19 15:11:05 +0530

    Change directory name

--- src/platform/web/theming/IconColorizer.ts
+++ src/platform/web/theming/IconColorizer.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 import type {Platform} from "../Platform.js";
-import {getColoredSvgString} from "./actions/svg-colorizer.mjs";
+import {getColoredSvgString} from "./shared/svg-colorizer.mjs";
 
 type ParsedStructure = {
     [variableName: string]: {
--- vite.common-config.js
+++ vite.common-config.js
@@ -9,7 +9,7 @@ const manifest = require("./package.json");
 const version = manifest.version;
 const compiledVariables = new Map();
 import {buildColorizedSVG as replacer} from "./scripts/postcss/svg-builder.mjs";
-import {derive} from "./src/platform/web/theming/actions/color.mjs";
+import {derive} from "./src/platform/web/theming/shared/color.mjs";
 
 const commonOptions = {
     logLevel: "warn",
