7ac2c7c7faff035248c1e8d6fdc5ce771b3d3784
RMidhunSuresh <hi@midhun.dev>
2022-07-19 21:06:55 +0530

    Get tests to work

--- src/platform/web/theming/shared/color.mjs
+++ src/platform/web/theming/shared/color.mjs
@@ -13,7 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import {offColor} from 'off-color';
+import pkg from 'off-color';
+const offColor = pkg.offColor;
 
 export function derive(value, operation, argument, isDark) {
     const argumentAsNumber = parseInt(argument);
