3258f1c64b5588958b2fc74d3418ec197aced70f
Kaki In <91763754+Kaki-In@users.noreply.github.com>
2022-07-29 16:45:12 +0200

    Updated emoji use

--- src/platform/web/ui/general/emoji/EmojisDirectory.js
+++ src/platform/web/ui/general/emoji/EmojisDirectory.js
@@ -2,82 +2,80 @@ import {Emoji} from "./Emoji";
 import {default_emojis} from "./emojis";
 
 export class EmojisDirectory {
-	constructor() {
-		this._emojisList = new Map();
-		this._symbol = null;
-		this._emojiclass = Emoji;
-	}
-	
-	init() {
-		let emojiIndex = 0;
-		for () {
-			for (const emojilist of emojis) {
-				
-				this.addEmoji(new Emoji(...emoji));
+    constructor() {
+        this._emojisList = new Map();
+        this._symbol = null;
+        this._emojiclass = Emoji;
+    }
+    
+    init() {
+		for (const emojilist of emojis) {
+			for (const emoji of emojilist) {
+				this.addEmoji(new Emoji(emoji[0], emoji[1], emojilist[0]));
 			}
 		}
-		this.setSeparateSymbol(':');
-	}
-	
-	addEmoji(emoji) {
-		this._emojisList.set(emoji.name, emoji);
-	}
+        this.setSeparateSymbol(':');
+    }
+    
+    addEmoji(emoji) {
+        this._emojisList.set(emoji.name, emoji);
+    }
 
-	getEmoji(emojiName) {
-		return this._emojisList.get(emojiName);
-	}
+    getEmoji(emojiName) {
+        return this._emojisList.get(emojiName);
+    }
 
-	deleteEmoji(emojiName) {
-		return this._emojisList.delete(emojiName);
-	}
-	
-	setSeparateSymbol(symbol) {
-		this._symbol = symbol;
-	}
+    deleteEmoji(emojiName) {
+        return this._emojisList.delete(emojiName);
+    }
+    
+    setSeparateSymbol(symbol) {
+        this._symbol = symbol;
+    }
 
-	get separateSymbol () {
-		return this._symbol;
-	}
+    get separateSymbol () {
+        return this._symbol;
+    }
 
-	parseEmojis(text) {
-		let ret = "";
-		let n = 0;
-		let lastWasEmoji = true; // no double points available
-		for (const part of text.split(this._symbol)) {
-			n++;
-			if (!lastWasEmoji) { // because the double points belongs to emojis. If the last one was an emoji, the actual part is not an emoji.
-				if (part.includes(" ")) {
-					ret += this._symbol + part;
-					lastWasEmoji = false;
-					continue; // in "I am :really happy:happy:", we don't replace "really happy". We always consider that the next one can be an emoji.
-				} 
-				for (const emoji of this._emojisList.keys()) {
-					if (emoji == part) {
-						ret += this._emojisList.get(part).emoji;
-						lastWasEmoji = true;
-					}
-				}
-				if (!lastWasEmoji) { // no emoji found, consider as it was two single double points.
-					ret += (n !== 1 && !lastWasEmoji ? this._symbol : "") + part;
-					lastWasEmoji = false;
-				}
-			} else {
-				ret += (n !== 1 && !lastWasEmoji ? this._symbol : "") + part;// if the last was an emoji, the double points shouldn't been added, they're part of the emoji.
-				lastWasEmoji = false;
-			}
-		}
-		console.log(text, ret);
-		return ret;
-	}
+    parseEmojis(text) {
+        let ret = "";
+        let n = 0;
+        let lastWasEmoji = true; // no double points available
+        for (const part of text.split(this._symbol)) {
+            n++;
+            if (!lastWasEmoji) { // because the double points belongs to emojis. If the last one was an emoji, the actual part is not an emoji.
+                if (part.includes(" ")) {
+                    ret += this._symbol + part;
+                    lastWasEmoji = false;
+                    continue; // in "I am :really happy:happy:", we don't replace "really happy". We always consider that the next one can be an emoji.
+                } 
+                for (const emoji of this._emojisList.keys()) {
+                    if (emoji == part) {
+                        ret += this._emojisList.get(part).emoji;
+                        lastWasEmoji = true;
+                    }
+                }
+                if (!lastWasEmoji) { // no emoji found, consider as it was two single double points.
+                    ret += (n !== 1 && !lastWasEmoji ? this._symbol : "") + part;
+                    lastWasEmoji = false;
+                }
+            } else {
+                ret += (n !== 1 && !lastWasEmoji ? this._symbol : "") + part;// if the last was an emoji, the double points shouldn't been added, they're part of the emoji.
+                lastWasEmoji = false;
+            }
+        }
+        console.log(text, ret);
+        return ret;
+    }
 
-	searchEmojis(emojiName) {
-		let ret = new Array();
-		for (const emoname of this._emojisList.keys()) {
-			if (emoname.includes(emojiName)) {
-				ret.push(this._emojisList.get(emoname));
-			}
-		}
-		return ret;
-	}
+    searchEmojis(emojiName) {
+        let ret = new Array();
+        for (const emoname of this._emojisList.keys()) {
+            if (emoname.includes(emojiName)) {
+                ret.push(this._emojisList.get(emoname));
+            }
+        }
+        return ret;
+    }
 
 }
--- src/platform/web/ui/general/emoji/emojis.js
+++ src/platform/web/ui/general/emoji/emojis.js
@@ -2,85 +2,83 @@
 
 // The emojis array defines the types names and their emojis list
 
-export const emojis = [
-	["Face smiling", face_smiling], 
-	"Face affection", 
-	"Face tongue", 
-	"Face hand", 
-	"Face neutral skeptical", 
-	"Face sleepy", 
-	"Face unwell", 
-	"Face hat", 
-	"Face glasses", 
-	"Face concerned", 
-	"Face negative", 
-	"Face costume", 
-	"Cat face", 
-	"Monkey face", 
-	"Emotion", 
+export const default_emojis = [
+    ["Face smiling", emojis_face_smiling], 
+    ["Face affection", emojis_face_affection],
+    ["Face tongue", emojis_face_tongue],
+    ["Face hand", emojis_face_hand],
+    ["Face neutral skeptical", emojis_face_neutral_septikal], 
+    ["Face sleepy", emojis_face_sleepy],
+    ["Face unwell", emojis_face_unwell],
+    ["Face hat", emojis_face_hat],
+    ["Face glasses", emojis_face_glasses], 
+    ["Face concerned", emojis_face_concerned], 
+    ["Face negative", emojis_face_negative], 
+    ["Face costume", emojis_face_costume],
+    ["Cat face", emojis_cat_face], 
+    ["Monkey face", emojis_monkey_face],
+    ["Emotion", emojis_emotion]
 ]
 
 // thos are the emoji lists
 
-export const face_smiling = [
-	[ '😀', "grinning" ],
-	[ '😃', "smiley" ],
-	[ '😄', "smile" ],
-	[ '😁', "grin" ],
-	[ '😆', "laughing" ],
-	[ '😅', "sweat_smile" ],
-	[ '🤣', "rolling_on_the_floor_laughing" ],
-	[ '😂', "joy" ],
-	[ '🙂', "slightly_smilling_face" ],
-	[ '🙃', "upside_face" ],
-	[ '🫠', "melting_face" ],
-	[ '😉', "wink" ]
-	[ '😉', "blsuh" ]
-	[ '😇', "innocent" ]
+export const emojis_face_smiling = [
+    [ '😀', "grinning" ],
+    [ '😃', "smiley" ],
+    [ '😄', "smile" ],
+    [ '😁', "grin" ],
+    [ '😆', "laughing" ],
+    [ '😅', "sweat_smile" ],
+    [ '🤣', "rolling_on_the_floor_laughing" ],
+    [ '😂', "joy" ],
+    [ '🙂', "slightly_smilling_face" ],
+    [ '🙃', "upside_face" ],
+    [ '🫠', "melting_face" ],
+    [ '😉', "wink" ]
+    [ '😉', "blsuh" ]
+    [ '😇', "innocent" ]
 ]
 
-export const face_affection = [
+export const emojis_face_affection = [
 ]
 
-export const face_tongue = [
+export const emojis_face_tongue = [
 ]
 
-export const face_hand = [
+export const emojis_face_hand = [
 ]
 
-export const face_neutral_skeptical = [
+export const emojis_face_neutral_skeptical = [
 ]
 
-export const face_sleepy = [
+export const emojis_face_sleepy = [
 ]
 
-export const face_unwell = [
+export const emojis_face_unwell = [
 ]
 
-export const face_hat = [
+export const emojis_face_hat = [
 ]
 
-export const face_glasses = [
+export const emojis_face_glasses = [
 ]
 
-export const face_concerned = [
+export const emojis_face_concerned = [
 ]
 
-export const face_negative = [
+export const emojis_face_negative = [
 ]
 
-export const face_costume = [
+export const emojis_face_costume = [
 ]
 
-export const cat_face = [
+export const emojis_cat_face = [
 ]
 
-export const monkey_face = [
+export const emojis_monkey_face = [
 ]
 
-export const emotion = [
+export const emojis_emotion = [
 ]
 
-export const emotion = [
-]
 
