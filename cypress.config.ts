193415ac1d4d93875f19a5640d758a140011b266
RMidhunSuresh <hi@midhun.dev>
2022-09-06 01:25:28 +0530

    Add configurable variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
             require("./cypress/plugins/index.ts").default(on, config);
             return config;
         },
-        baseUrl: "http://localhost:3000",
+        baseUrl: "http://127.0.0.1:3000",
+    },
+    env: {
+        SYNAPSE_IP_ADDRESS: "172.18.0.5",
+        SYNAPSE_PORT: "8008",
+        DEX_IP_ADDRESS: "172.18.0.4",
+        DEX_PORT: "5556",
     },
 });
