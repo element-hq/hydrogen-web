dcd514a4845a00c863e68b553815526d6d471a82
Paulo Pinto <paulo.pinto@automattic.com>
2023-01-09 15:55:24 +0000

    Center UnknownRoomView content vertically

--- src/platform/web/ui/session/room/UnknownRoomView.js
+++ src/platform/web/ui/session/room/UnknownRoomView.js
@@ -24,17 +24,19 @@ export class UnknownRoomView extends TemplateView {
                 t.h2("Join room"),
             ]),
             t.div({className: "UnknownRoomView_body centered-column"}, [
-                t.h2([
-                    vm.i18n`You are currently not in ${vm.roomIdOrAlias}.`,
-                    t.br(),
-                    vm.i18n`Want to join it?`
-                ]),
-                t.button({
-                    className: "button-action primary",
-                    onClick: () => vm.join(),
-                    disabled: vm => vm.busy,
-                }, vm.i18n`Join room`),
-                t.if(vm => vm.error, t => t.p({className: "error"}, vm.error))
+                t.div({className: "UnknownRoomView_container"}, [
+                    t.h2([
+                        vm.i18n`You are currently not in ${vm.roomIdOrAlias}.`,
+                        t.br(),
+                        vm.i18n`Want to join it?`
+                    ]),
+                    t.button({
+                        className: "button-action primary",
+                        onClick: () => vm.join(),
+                        disabled: vm => vm.busy,
+                    }, vm.i18n`Join room`),
+                    t.if(vm => vm.error, t => t.p({className: "error"}, vm.error))
+                ])
             ])
         ]);
     }
