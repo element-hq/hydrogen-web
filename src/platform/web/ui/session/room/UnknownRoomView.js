7d1d6c73ee8c90913ae19b04bedba71c89f62417
Paulo Pinto <paulo.pinto@automattic.com>
2023-01-09 15:33:12 +0000

    Wrap content of JoinRoomView in a body

--- src/platform/web/ui/session/room/UnknownRoomView.js
+++ src/platform/web/ui/session/room/UnknownRoomView.js
@@ -18,18 +18,20 @@ import {TemplateView} from "../../general/TemplateView";
 
 export class UnknownRoomView extends TemplateView {
     render(t, vm) {
-        return t.main({className: "UnknownRoomView middle"}, t.div([
-            t.h2([
-                vm.i18n`You are currently not in ${vm.roomIdOrAlias}.`,
-                t.br(),
-                vm.i18n`Want to join it?`
-            ]),
-            t.button({
-                className: "button-action primary",
-                onClick: () => vm.join(),
-                disabled: vm => vm.busy,
-            }, vm.i18n`Join room`),
-            t.if(vm => vm.error, t => t.p({className: "error"}, vm.error))
-        ]));
+        return t.main({className: "UnknownRoomView middle"}, [
+            t.div({className: "UnknownRoomView_body centered-column"}, [
+                t.h2([
+                    vm.i18n`You are currently not in ${vm.roomIdOrAlias}.`,
+                    t.br(),
+                    vm.i18n`Want to join it?`
+                ]),
+                t.button({
+                    className: "button-action primary",
+                    onClick: () => vm.join(),
+                    disabled: vm => vm.busy,
+                }, vm.i18n`Join room`),
+                t.if(vm => vm.error, t => t.p({className: "error"}, vm.error))
+            ])
+        ]);
     }
 }
