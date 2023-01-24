e3a8c184f6088d57f49bd9584e06df8a06d26eb9
RMidhunSuresh <hi@midhun.dev>
2023-01-24 12:03:04 +0530

    Fix formatting

--- src/domain/session/toast/BaseToastNotificationViewModel.ts
+++ src/domain/session/toast/BaseToastNotificationViewModel.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { ErrorReportViewModel } from "../../ErrorReportViewModel";
+import {ErrorReportViewModel} from "../../ErrorReportViewModel";
 import {Options as BaseOptions} from "../../ViewModel";
 import type {Session} from "../../../matrix/Session.js";
 
