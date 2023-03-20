4b8db134820c36bd3ba7f666c3c47491b50b41ae
Robert Long <robert@robertlong.me>
2023-03-20 14:11:51 -0700

    Export FeatureSet/FeatureFlag

--- src/lib.ts
+++ src/lib.ts
@@ -19,6 +19,7 @@ export type {ILogItem} from "./logging/types";
 export {IDBLogPersister} from "./logging/IDBLogPersister";
 export {ConsoleReporter} from "./logging/ConsoleReporter";
 export {Platform} from "./platform/web/Platform.js";
+export {FeatureSet, FeatureFlag} from "./features.js";
 export {Client, LoadStatus, LoginFailure} from "./matrix/Client.js";
 export {RoomStatus} from "./matrix/room/common";
 export {AttachmentUpload} from "./matrix/room/AttachmentUpload";
